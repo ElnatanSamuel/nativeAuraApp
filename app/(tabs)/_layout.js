13d65e545d29b8453db498ee1f598adbfe4f690f
ElnatanSamuel <elnatan23samuel@gmail.com>
2024-04-23 22:41:03 +0300

    full app

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,35 +1,42 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
-import { icons } from "../../constants";
+import { StatusBar } from "expo-status-bar";
+import { Redirect, Tabs } from "expo-router";
+import { Image, Text, View } from "react-native";
 
-const TabsLayout = () => {
-  const TabIcon = ({ icon, color, name, focused }) => {
-    return (
-      <View className="items-center justify-center gap-1">
-        <Image
-          source={icon}
-          resizeMode="contain"
-          tintColor={color}
-          className="h-5 w-5"
-        />
-        <Text
-          className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
-          style={{ color: color }}
-        >
-          {name}
-        </Text>
-      </View>
-    );
-  };
+import { icons } from "../../constants";
+import { Loader } from "../../components";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabLayout = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && !isLogged) return <Redirect href="/sign-in" />;
 
   return (
     <>
       <Tabs
         screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
           tabBarShowLabel: false,
-          tabBarActiveTintColor: "#ffa001",
-          tabBarInactiveTintColor: "#cdcde0",
           tabBarStyle: {
             backgroundColor: "#161622",
             borderTopWidth: 1,
@@ -53,7 +60,6 @@ const TabsLayout = () => {
             ),
           }}
         />
-
         <Tabs.Screen
           name="bookmark"
           options={{
@@ -101,8 +107,11 @@ const TabsLayout = () => {
           }}
         />
       </Tabs>
+
+      <Loader isLoading={loading} />
+      <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
 };
 
-export default TabsLayout;
+export default TabLayout;
--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
-import React, { useEffect } from "react";
-import { Stack, SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
     "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
@@ -25,13 +29,23 @@ const RootLayout = () => {
     }
   }, [fontsLoaded, error]);
 
+  if (!fontsLoaded) {
+    return null;
+  }
+
   if (!fontsLoaded && !error) {
     return null;
   }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 };
 
